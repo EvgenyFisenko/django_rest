3de5008f7a5455c70109292bbd3c4bf88665693d
Evgeny Fisenko <e.fisenko@outlook.com>
2020-10-26 17:12:51 +0300

    initial

---
+++ movies/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from .models import Movie, Review
+
+
+class MovieListSerializer(serializers.ModelSerializer):
+    """ список фильмов """
+    class Meta:
+        model = Movie
+        fields = ('title', 'tagline', 'category')
+
+
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    """ добавление отзыва """
+
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """ добавление отзыва """
+
+    class Meta:
+        model = Review
+        fields = ('name', 'text', 'parent')
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    """ фильм """
+    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    directors = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    actors = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    genres = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    reviews = ReviewSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        exclude = ('draft', )
